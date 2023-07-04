777e22912943656c5597aa6469fcb388c4561627
AlessandroPolidori <alessandro.polidori@orobix.com>
2023-07-04 14:09:13 +0000

    fix: Swap height and width in anomaly evaluation propagation

--- quadra/tasks/anomaly.py
+++ quadra/tasks/anomaly.py
@@ -337,19 +337,19 @@ class AnomalibEvaluation(Task[AnomalyDataModule]):
         if not isinstance(self.model_data, dict):
             raise ValueError("Model info file is not a valid json")
 
-        if self.model_data["input_size"][0] != self.config.transforms.input_height:
+        if self.model_data["input_size"][0] != self.config.transforms.input_width:
             log.warning(
-                f"Input height of the model ({self.model_data['input_size'][0]}) is different from the one specified "
-                + f"in the config ({self.config.transforms.input_height}). Fixing the config."
-            )
-            self.config.transforms.input_height = self.model_data["input_size"][0]
-
-        if self.model_data["input_size"][1] != self.config.transforms.input_width:
-            log.warning(
-                f"Input width of the model ({self.model_data['input_size'][1]}) is different from the one specified "
+                f"Input width of the model ({self.model_data['input_size'][0]}) is different from the one specified "
                 + f"in the config ({self.config.transforms.input_width}). Fixing the config."
             )
-            self.config.transforms.input_width = self.model_data["input_size"][1]
+            self.config.transforms.input_width = self.model_data["input_size"][0]
+
+        if self.model_data["input_size"][1] != self.config.transforms.input_height:
+            log.warning(
+                f"Input height of the model ({self.model_data['input_size'][1]}) is different from the one specified "
+                + f"in the config ({self.config.transforms.input_height}). Fixing the config."
+            )
+            self.config.transforms.input_height = self.model_data["input_size"][1]
 
         self.deployment_model = self.model_path
 
