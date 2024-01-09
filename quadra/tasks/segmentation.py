fix: Segmentation evaluation half precision fixes

quadra/tasks/segmentation.py
@@ -304,7 +304,7 @@ class SegmentationAnalysisEvaluation(SegmentationEvaluation):
         self,
         config: DictConfig,
         model_path: str,
-        device: Optional[str] = "cpu",
+        device: Optional[str] = None,
     ):
         super().__init__(config=config, model_path=model_path, device=device)
         self.test_output: Dict[str, Any] = {}
@@ -341,13 +341,17 @@ class SegmentationAnalysisEvaluation(SegmentationEvaluation):
             for batch in dataloader:
                 images, masks, labels = batch
                 images = images.to(self.device)
+                if self.half_precision:
+                    images = images.half()
+                elif self.half_precision is None:
+                    raise ValueError("Half_precision bool attribute has not been set. Check super class prepare()")
                 if len(masks.shape) == 3:  # BxHxW -> Bx1xHxW
                     masks = masks.unsqueeze(1)
                 with torch.no_grad():
-                    image_list.append(images.cpu())
-                    mask_list.append(masks.cpu())
-                    mask_pred_list.append(self.deployment_model(images.to(self.device)).cpu())
-                    label_list.append(labels.cpu())
+                    image_list.append(images)
+                    mask_list.append(masks)
+                    mask_pred_list.append(self.deployment_model(images.to(self.device)))
+                    label_list.append(labels)
 
             output = {
                 "image": torch.cat(image_list, dim=0),